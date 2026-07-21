a3b991a2cabdd98c2df8bc2809023cf2b0f0532b
cactusmultimedia <gavota_enlatado.1u@icloud.com>
2026-07-21 15:34:48 -0600

    Add 9 Celia photos from DSC (5364/5408-411/5433/5472-473/5475)

--- datos_primaria.js
+++ datos_primaria.js
@@ -3,7 +3,7 @@ const ninos = [
   { id: 2, codigo: "S370", caption: "Ariadna", fotos: ["2/ariadna-paquete1.jpg","2/ariatna.JPG"] },
   { id: 3, codigo: "S444", caption: "Bruno", fotos: ["3/bruno-paquete1.jpg","3/bruno.JPG"] },
   { id: 4, codigo: "S365", caption: "Carlos", fotos: ["4/carlos-paquete1.jpg","4/carlos.JPG"] },
-  { id: 5, codigo: "S858", caption: "Celia", fotos: ["5/celia-paquete1.jpg","5/celia.JPG"] },
+  { id: 5, codigo: "S858", caption: "Celia", fotos: ["5/celia-paquete1.jpg","5/celia.JPG","5/celiaDSC_5364.JPG","5/celiaDSC_5408.JPG","5/celiaDSC_5409.JPG","5/celiaDSC_5410.JPG","5/celiaDSC_5411.JPG","5/celiaDSC_5433.JPG","5/celiaDSC_5472.JPG","5/celiaDSC_5473.JPG","5/celiaDSC_5475.JPG"] },
   { id: 6, codigo: "S545", caption: "Elizabeth", fotos: ["6/elizabeth-paquete1.jpg","6/elizabeth.JPG"] },
   { id: 7, codigo: "S401", caption: "Emily", fotos: ["7/emily-paquete1.jpg"] },
   { id: 8, codigo: "S640", caption: "Hector Gael", fotos: ["8/hector.JPG","8/hectorgael-paquete1.jpg"] },
