058ba74f35a382be07f2c92d0cc8f3649de0d8a8
cactusmultimedia <gavota_enlatado.1u@icloud.com>
2026-07-21 15:39:32 -0600

    Add portadas array and fotos-graduacion-primaria/5 portadas for Celia

--- datos_primaria.js
+++ datos_primaria.js
@@ -1,19 +1,28 @@
 const ninos = [
-  { id: 1, codigo: "S229", caption: "Alan", fotos: ["1/alan-paquete2.jpg","1/alan.JPG"] },
-  { id: 2, codigo: "S370", caption: "Ariadna", fotos: ["2/ariadna-paquete1.jpg","2/ariatna.JPG"] },
-  { id: 3, codigo: "S444", caption: "Bruno", fotos: ["3/bruno-paquete1.jpg","3/bruno.JPG"] },
-  { id: 4, codigo: "S365", caption: "Carlos", fotos: ["4/carlos-paquete1.jpg","4/carlos.JPG"] },
-  { id: 5, codigo: "S858", caption: "Celia", fotos: ["5/celia-paquete1.jpg","5/celia.JPG","5/celiaDSC_5364.JPG","5/celiaDSC_5408.JPG","5/celiaDSC_5409.JPG","5/celiaDSC_5410.JPG","5/celiaDSC_5411.JPG","5/celiaDSC_5433.JPG","5/celiaDSC_5472.JPG","5/celiaDSC_5473.JPG","5/celiaDSC_5475.JPG"] },
-  { id: 6, codigo: "S545", caption: "Elizabeth", fotos: ["6/elizabeth-paquete1.jpg","6/elizabeth.JPG"] },
-  { id: 7, codigo: "S401", caption: "Emily", fotos: ["7/emily-paquete1.jpg"] },
-  { id: 8, codigo: "S640", caption: "Hector Gael", fotos: ["8/hector.JPG","8/hectorgael-paquete1.jpg"] },
-  { id: 9, codigo: "S309", caption: "Itzayana", fotos: ["9/itzayana-paquete2.jpg","9/itzayana.JPG"] },
-  { id: 10, codigo: "S414", caption: "Leah", fotos: ["10/lea.JPG","10/leah-paquete2.jpg"] },
-  { id: 11, codigo: "S413", caption: "Mia", fotos: ["11/mia-paquete2.jpg","11/mia.JPG"] },
-  { id: 12, codigo: "S315", caption: "Montserrath", fotos: ["12/monserrat.JPG","12/montserrath-paquete1.jpg"] },
-  { id: 13, codigo: "S692", caption: "Nahomi", fotos: ["13/nahomi aguilar.JPG","13/nahomi-paquete2.jpg"] },
-  { id: 14, codigo: "S984", caption: "Nahum", fotos: ["14/nahum-paquete1.jpg","14/nahum.JPG","14/nahumDSC_5272.JPG","14/nahumDSC_5273.JPG","14/nahumDSC_5274.JPG","14/nahumDSC_5275.JPG"] },
-  { id: 15, codigo: "S558", caption: "Naomi", fotos: ["15/naomi-paquete1.jpg","15/naomi.JPG"] },
-  { id: 16, codigo: "S953", caption: "Ximena", fotos: ["16/ximena-paquete1.jpg","16/ximena.JPG"] },
-  { id: 17, codigo: "S238", caption: "Yoarp", fotos: ["17/yoarp-paquete1.jpg","17/yoarp.JPG"] }
+{ id: 1, codigo: "S229", caption: "Alan", fotos: ["1/alan-paquete2.jpg","1/alan.JPG"] },
+{ id: 2, codigo: "S370", caption: "Ariadna", fotos: ["2/ariadna-paquete1.jpg","2/ariatna.JPG"] },
+{ id: 3, codigo: "S444", caption: "Bruno", fotos: ["3/bruno-paquete1.jpg","3/bruno.JPG"] },
+{ id: 4, codigo: "S365", caption: "Carlos", fotos: ["4/carlos-paquete1.jpg","4/carlos.JPG"] },
+{ id: 5, codigo: "S858", caption: "Celia", fotos: ["5/celia-paquete1.jpg","5/celia.JPG","5/celiaDSC_5364.JPG","5/celiaDSC_5408.JPG","5/celiaDSC_5409.JPG","5/celiaDSC_5410.JPG","5/celiaDSC_5411.JPG","5/celiaDSC_5433.JPG","5/celiaDSC_5472.JPG","5/celiaDSC_5473.JPG","5/celiaDSC_5475.JPG"] },
+{ id: 6, codigo: "S545", caption: "Elizabeth", fotos: ["6/elizabeth-paquete1.jpg","6/elizabeth.JPG"] },
+{ id: 7, codigo: "S401", caption: "Emily", fotos: ["7/emily-paquete1.jpg"] },
+{ id: 8, codigo: "S640", caption: "Hector Gael", fotos: ["8/hector.JPG","8/hectorgael-paquete1.jpg"] },
+{ id: 9, codigo: "S309", caption: "Itzayana", fotos: ["9/itzayana-paquete2.jpg","9/itzayana.JPG"] },
+{ id: 10, codigo: "S414", caption: "Leah", fotos: ["10/lea.JPG","10/leah-paquete2.jpg"] },
+{ id: 11, codigo: "S413", caption: "Mia", fotos: ["11/mia-paquete2.jpg","11/mia.JPG"] },
+{ id: 12, codigo: "S315", caption: "Montserrath", fotos: ["12/monserrat.JPG","12/montserrath-paquete1.jpg"] },
+{ id: 13, codigo: "S692", caption: "Nahomi", fotos: ["13/nahomi aguilar.JPG","13/nahomi-paquete2.jpg"] },
+{ id: 14, codigo: "S984", caption: "Nahum", fotos: ["14/nahum-paquete1.jpg","14/nahum.JPG","14/nahumDSC_5272.JPG","14/nahumDSC_5273.JPG","14/nahumDSC_5274.JPG","14/nahumDSC_5275.JPG"] },
+{ id: 15, codigo: "S558", caption: "Naomi", fotos: ["15/naomi-paquete1.jpg","15/naomi.JPG"] },
+{ id: 16, codigo: "S953", caption: "Ximena", fotos: ["16/ximena-paquete1.jpg","16/ximena.JPG"] },
+{ id: 17, codigo: "S238", caption: "Yoarp", fotos: ["17/yoarp-paquete1.jpg","17/yoarp.JPG"] }
+];
+
+const portadas = [
+  null,null,null,null,
+  "5/celia-paquete1.jpg",
+  null,null,null,null,
+  null,null,null,null,
+  null,null,null,null,
+  null
 ];
